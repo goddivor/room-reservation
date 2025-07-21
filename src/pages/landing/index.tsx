f8ce6955659df2ec2781df0dc7b6ea1171c3f762
Divor <67099009+goddivor@users.noreply.github.com>
2025-07-21 13:20:50 +0000

    feat: integrate all landing sections in main page

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import AboutSection from "@/components/landing/AboutSection";
+import ContactSection from "@/components/landing/ContactSection";
+import Footer from "@/components/landing/Footer";
+import HeroSection from "@/components/landing/HeroSection";
+import Navbar from "@/components/landing/NavBar";
+import NewsletterSection from "@/components/landing/NewsLetterSection";
+import SearchSection from "@/components/landing/SearchSection";
+import TestimonialsSection from "@/components/landing/TestimonialsSection";
+import RoomSection from "@/components/room-config/RoomSection";
+import React from "react";
 
 const LandingPage: React.FC = () => {
-    return (
-        <div>
-            <h1>Welcome to the Room Reservation App</h1>
-            <p>Get started by selecting an option from the menu.</p>
-        </div>
-    );
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <HeroSection />
+      <SearchSection />
+      <RoomSection />
+      <AboutSection />
+      <TestimonialsSection />
+      <ContactSection />
+      <NewsletterSection />
+      <Footer />
+    </div>
+  );
 };
 
 export default LandingPage;
